ca95765364f9e6ce86381d1140e559e3b3247f03
Minh Truong <minhtd@Truongs-MacBook-Pro.local>
2022-06-09 09:23:59 +0700

    update api url

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth/';
+const API_URL = 'https://mighty-thicket-69286.herokuapp.com/api/auth/';
 const signup = (username, fullName, email, userCode, department, password) => {
   const data = {
     username,
